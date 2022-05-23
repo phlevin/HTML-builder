e8f80b776eb1bd250bafb582d50f175fe2147fa4
Phill Levin <phill@levin.tk>
2022-05-23 19:18:00 +0300

    feat: second part done

---
+++ 02-write-file/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, exit } = process;
+
+const filePath = path.join(__dirname, 'textFile.txt');
+const output = fs.createWriteStream(filePath);
+
+console.log('Hello! Please enter the text!\n');
+
+stdin.on('data', data => {
+  if (data.toString().trim() === 'exit') {
+    exit();
+  }
+  output.write(data);
+});
+
+process.on('exit', () => console.log('Goodbye! Good luck!'));
+process.on('SIGINT', exit);
