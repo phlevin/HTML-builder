f06ef8543403c25d721d46d5894a41643c6186d7
Phill Levin <phill@levin.tk>
2022-05-23 20:04:01 +0300

    feat: fourth part done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fs = require('fs');
+const path = require('path');
 
+const pathToFolder = path.join(__dirname, 'files');
+const pathToFolderCopy = path.join(__dirname, 'files-copy');
+
+function copyDir(folder, folderCopy) {
+  fs.readdir(folder, (error, files) => {
+    files.forEach((file) => {
+      fs.readFile(path.join(folder, file), 'utf-8', (error, data) => {
+        if (error) {
+          throw error;
+        }
+        fs.mkdir(folderCopy, { recursive: true }, () => {
+          fs.writeFile(path.join(folderCopy, file), data, (error) => {
+            if (error) {
+              throw error;
+            }
+          });
+        });
+      });
+    });
+  });
+}
+
+copyDir(pathToFolder, pathToFolderCopy);
